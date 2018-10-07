2369a2c6f1eb77690cd4ae745713a8e03706427d
Garel Bucknor <garelbucknor@garels-mac-mini.local>
2018-10-06 18:45:22 -0700

    Initial Commit

---
+++ Lexicon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lexicon
+//
+//  Created by Garel Bucknor on 2018-10-06.
+//  Copyright © 2018 Garel Bucknor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
