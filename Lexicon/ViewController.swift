918cad9076d3be59ebb8e95d25988b94745c8530
Garel Bucknor <a00961922@imac-97.ad.bcit.ca>
2018-11-23 12:07:58 -0800

    Added proper game logic and score tracking.

--- Lexicon/ViewController.swift
+++ Lexicon/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        displayScore()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        displayScore()
+    }
+    
+    private func displayScore() {
+        let score = UserDefaults.standard.value(forKey: "score") ?? nil
+        if score == nil {
+            UserDefaults.standard.set(0, forKey: "score")
+        } else if score as! Int != 0 {
+            scoreLabel.text = "High score: " + (String(score as! Int)) + "pts!"
+        }
     }
 
     @IBAction func wordGuess(_ sender: Any) {
--- Lexicon/WGGameViewController.swift
+++ Lexicon/WGGameViewController.swift
@@ -14,23 +14,58 @@ class WGGameViewController: UIViewController {
     var wordArray: [NSDictionary] = []
     var category: String = ""
     var word: String = ""
+    var seconds: Int = 15
+    var timer: Timer = Timer()
+    var currentScore: Int = 0
     
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var mysteryDescription: UILabel!
     @IBOutlet weak var guessArea: UITextField!
-    
     @IBOutlet weak var categoryTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         categoryTitle.text = category
+        guessArea.spellCheckingType = UITextSpellCheckingType.no
+        setUpGuessArea()
+    }
+    
+    // Creates a timer for the current match.
+    private func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(self.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    // Updates the number of seconds left for a guess.
+    // Resets the time if the seconds get to 0.
+    @objc private func updateTimer() {
+        seconds -= 1
+        timerLabel.text = "\(seconds)"
+        if (seconds == 5) {
+            timerLabel.textColor = UIColor.red
+        }
+        if (seconds <= 0) {
+            timer.invalidate()
+            setUpGuessArea()
+        }
+    }
+    
+    // Sets up the ui of the game.
+    private func setUpGuessArea() {
+        seconds = 15
+        timerLabel.textColor = UIColor.blue
+        timerLabel.text = "\(seconds)"
         word = getRandomWord()
         print(word)
         setDescription()
-        guessArea.spellCheckingType = UITextSpellCheckingType.no
         guessArea.text = ""
+        runTimer()
     }
     
+    /* API REQUEST FUNCTIONS */
+    
+    // Creates an api request for the dictionary definition of the currently selected word.
     private func setDescription() {
         let appId = "52a4a509"
         let appKey = "0e4140c7beac18571782bb7039b8f2da"
@@ -44,6 +79,7 @@ class WGGameViewController: UIViewController {
         getDefReq(request)
     }
     
+    // Parses a JSON response as an array of dictionary objects.
     private func getDefReq(_ request : URLRequest) {
         let session = URLSession.shared
         _ = session.dataTask(with: request, completionHandler: { data, response, error in
@@ -59,6 +95,7 @@ class WGGameViewController: UIViewController {
         }).resume()
     }
     
+    // Searches for word definitions through dictionary objects.
     private func getDef(_ results : [NSDictionary]) {
         let l_entries = results[0].value(forKey: "lexicalEntries") as! [NSDictionary]
         let entries = l_entries[0].value(forKey: "entries") as! [NSDictionary]
@@ -70,11 +107,13 @@ class WGGameViewController: UIViewController {
                 self.mysteryDescription.numberOfLines = 4
             }
         } else {
-            word = getRandomWord()
-            setDescription()
+            setUpGuessArea()
         }
     }
     
+    /* END API REQUEST FUNCTIONS (thanks Oxford) */
+    
+    // Picks a random word from the currently set category array.
     private func getRandomWord() -> String {
         var temp = wordArray[Int.random(in: 0 ..< wordArray.count)].value(forKey: "word") as! String
         while (temp.contains(",")) {
@@ -83,30 +122,77 @@ class WGGameViewController: UIViewController {
         return temp
     }
     
+    // Shakes the textfield
+    private func shakeAnimation() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 2
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: guessArea.center.x - 10, y: guessArea.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: guessArea.center.x + 10, y: guessArea.center.y))
+        guessArea.layer.add(animation, forKey: "position")
+        guessArea.text = ""
+    }
+    
+    // Closes the current view
     @IBAction func quitPressed(_ sender: Any) {
+        timer.invalidate()
         dismiss(animated: true, completion: nil)
     }
+    
+    // Checks guesses
     @IBAction func checkGuess(_ sender: Any) {
         if ((guessArea.text?.lowercased().elementsEqual(word))!) {
-            viewDidLoad()
+            timer.invalidate()
+            setUpGuessArea()
+            currentScore += 1
+            let score = UserDefaults.standard.integer(forKey: "score")
+            if currentScore > score {
+                UserDefaults.standard.set(score + 1, forKey: "score")
+            }
+            showToast(message: "+1")
         } else {
-            let animation = CABasicAnimation(keyPath: "position")
-            animation.duration = 0.05
-            animation.repeatCount = 2
-            animation.autoreverses = true
-            animation.fromValue = NSValue(cgPoint: CGPoint(x: guessArea.center.x - 10, y: guessArea.center.y))
-            animation.toValue = NSValue(cgPoint: CGPoint(x: guessArea.center.x + 10, y: guessArea.center.y))
-            guessArea.layer.add(animation, forKey: "position")
-            guessArea.text = ""
+            shakeAnimation()
         }
     }
 }
 
+// Converts certain special characters to rfc3986 for api calls.
 extension String {
     func stringByAddingPercentEncodingForRFC3986() -> String? {
         let unreserved = "-._~/?"
         let allowed = NSMutableCharacterSet.alphanumeric()
         allowed.addCharacters(in: unreserved)
         return addingPercentEncoding(withAllowedCharacters: allowed as CharacterSet)
+    }
+}
+
+// Displays toasts that slide upwards and then disappear.
+extension UIViewController {
+    func showToast(message: String) {
+        let toastLabel = UILabel(frame: CGRect(
+            x: self.view.frame.size.width/2 - 25,
+            y: self.view.frame.size.height-100,
+            width: 50,
+            height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0,
+                       delay: 0.1,
+                       options: .curveEaseOut,
+                       animations: {
+                        let top = CGAffineTransform(translationX: 0, y: -300)
+                        toastLabel.alpha = 0.0
+                        toastLabel.transform = top
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
 }
